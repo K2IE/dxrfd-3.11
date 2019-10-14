2a30b0395114cb0421976c038f5e149323e6ae88
Daniel L. Srebnick <dan@islenet.com>
2019-10-14 00:52:13 -0400

    Update to check for already running process.

--- dxrfd.cpp
+++ dxrfd.cpp
@@ -23,6 +23,12 @@ static regex_t preg;
 #include <sys/ioctl.h>
 #include <arpa/inet.h>
 
+/* K2IE - needed for check on dupe process */
+#include <dirent.h>
+#include <vector>
+#include <iostream>
+#include <fstream>
+
 /* Required for Binary search trees using C++ STL */
 #include <string>
 #include <map>
@@ -3815,6 +3821,51 @@ static void *playback(void *arg)
    pthread_exit(NULL);
 }
 
+int getProcIdByName(string procName)
+// From http://proswdev.blogspot.com/2012/02/get-process-id-by-name-in-linux-using-c.html
+{
+    int pid = -1;
+
+    // Open the /proc directory
+    DIR *dp = opendir("/proc");
+    if (dp != NULL)
+    {
+        // Enumerate all entries in directory until process found
+        struct dirent *dirp;
+        while (pid < 0 && (dirp = readdir(dp)))
+        {
+            // Skip non-numeric entries
+            int id = atoi(dirp->d_name);
+            if (id > 0)
+            {
+                // Read contents of virtual /proc/{pid}/cmdline file
+                string cmdPath = string("/proc/") + dirp->d_name + "/cmdline";
+                ifstream cmdFile(cmdPath.c_str());
+                string cmdLine;
+                getline(cmdFile, cmdLine);
+                if (!cmdLine.empty())
+                {
+                    // Keep first cmdline item which contains the program path
+                    size_t pos = cmdLine.find('\0');
+                    if (pos != string::npos)
+                        cmdLine = cmdLine.substr(0, pos);
+                    // Keep program name only, removing the path
+                    pos = cmdLine.rfind('/');
+                    if (pos != string::npos)
+                        cmdLine = cmdLine.substr(pos + 1);
+                    // Compare against requested process name
+                    if (procName == cmdLine)
+                        pid = id;
+                }
+            }
+        }
+    }
+
+    closedir(dp);
+
+    return pid;
+}
+
 int main(int argc, char **argv)
 {
    int rc = 0;
@@ -3830,9 +3881,11 @@ int main(int argc, char **argv)
       return 1;
    }
 
-   if (access(argv[2], F_OK) == 0)
+// Check to see if dxrfd is already running
+   int pid = getProcIdByName(argv[1]);
+   if (pid > 0)
    {
-      printf("Log file %s exists, is dxrfd already running?, if not running, remove log file and restart\n", argv[2]);
+      printf("dxrfd is already running.  Exiting...\n");
       return 1;
    }
 
